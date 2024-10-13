d0a27b84b0db8da37eea94a07c313ab1508ab118
Christopher Campbell <clccodes@yahoo.com>
2024-10-13 10:51:43 -0400

    Update to the Info component.

--- src/Info.jsx
+++ src/Info.jsx
@@ -7,8 +7,8 @@ export default function Info() {
             <p className="main--text">Foreign policy expert with 20+ years of experience, and a web developer. Fluent 
                 in French and Spanish, proficient in HTML, CSS, JavaScript, React, Python, and R.</p>
             <h2 className="main--title">Interests</h2>
-            <p className="main--text">An avid ice hockey fan, I love to watch hockey, play hockey, and 
-                am a certified on-ice official. I also enjoy coding and taking my skills to the next level.</p>
+            <p className="main--text">All things ice hockey. Fantasy football (soccer). A technology enthusiast, I love
+                 coding and thinking about how technology impacts our lives and can help solve problems.</p>
         </div>
     )
 }
